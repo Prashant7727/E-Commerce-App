078639ceefa7180a9d82f36106346248fe7aeaf1
Prashant7727 <prashant.sisodia08@gmail.com>
2024-05-16 19:53:25 +0530

    changes for images

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -155,7 +155,7 @@ const HomePage = () => {
             {products?.map((p) => (
               <div className="card m-2" key={p._id}>
                 <img
-                  src={`/api/v1/product/product-photo/${p._id}`}
+                  src={`https://e-commerce-backend-bice.vercel.app/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name}
                 />
